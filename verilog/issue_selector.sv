06519bb0824130b4912d0a06b21e8b47b3608e5a
Sheng-Yuan Chen <sychenn@caen-vnc-mi11.engin.umich.edu>
2025-10-23 20:14:27 -0400

    add Issue & FIFO

---
+++ verilog/issue_selector.sv
@@ -0,0 +1,89 @@
+// =========================================================
+// FU #0 = ALU0
+// FU #1 = ALU1
+// FU #2 = ALU2
+// FU #3 = MUL
+// FU #4 = LOAD
+// FU #5 = BRANCH
+// =========================================================
+
+module issue_selector #(
+    parameter int unsigned RS_DEPTH        = 64, // RS entry numbers
+    parameter int unsigned FU_NUM          = 6,  // total physical FUs
+    parameter int unsigned ISSUE_WIDTH     = 4
+)(
+    input  logic [FU_NUM-1:0]                 fu_fifo_full;, 
+    input  logic [RS_DEPTH-1:0]               rs_ready_vec,
+    input  logic [2:0]                        fu_types [RS_DEPTH], 
+    output logic [RS_DEPTH-1:0]               issue_rs_entry       
+)
+    int issue_cnt;
+    logic [FU_NUM-1:0]  tmp_fu_fifo_full;
+
+    always_comb begin
+        issue_rs_entry = '0;
+        issue_cnt      = 0;
+        tmp_fu_fifo_full      = fu_fifo_full;
+
+        // Iterate through RS entries in order
+        for (int i = 0; i < RS_DEPTH; i++) begin
+            if (issue_cnt >= ISSUE_WIDTH)
+                break;
+
+            if (rs_ready_vec[i]) begin
+                case (fu_types[i])
+
+                    // ALU: can go to FIFO 1, 2, or 3
+                    FU_ALU: begin
+                        if (!tmp_fu_fifo_full[0]) begin
+                            issue_rs_entry[i] = 1'b1;
+                            tmp_fu_fifo_full[0] = 1;
+                            issue_cnt++;
+                        end
+                        else if (!tmp_fu_fifo_full[1]) begin
+                            issue_rs_entry[i] = 1'b1;
+                            tmp_fu_fifo_full[1] =1;
+                            issue_cnt++;
+                        end
+                        else if (!tmp_fu_fifo_full[2]) begin
+                            issue_rs_entry[i] = 1'b1;
+                            tmp_fu_fifo_full[2] =1;
+                            issue_cnt++;
+                        end
+                    end
+
+                    // MUL → FIFO[4]
+                    FU_MUL: begin
+                        if (!tmp_fu_fifo_full[3]) begin
+                            issue_rs_entry[i] = 1'b1;
+                            tmp_fu_fifo_full[3] = 1;
+                            issue_cnt++;
+                        end
+                    end
+
+                    // LOAD → FIFO[5]
+                    FU_LOAD: begin
+                        if (!tmp_fu_fifo_full[4]) begin
+                            issue_rs_entry[i] = 1'b1;
+                            tmp_fu_fifo_full[4] = 1;
+                            issue_cnt++;
+                        end
+                    end
+
+                    // BRANCH → FIFO[6]
+                    FU_BRANCH: begin
+                        if (!tmp_fu_fifo_full[5]) begin
+                            issue_rs_entry[i] = 1'b1;
+                            tmp_fu_fifo_full[5] = 1;
+                            issue_cnt++;
+                        end
+                    end
+
+                    default: ;
+                endcase
+            end
+        end
+    end
+
+endmodule
+
--- verilog/rs.sv
+++ verilog/rs.sv
@@ -7,7 +7,7 @@
 //                                                                     //
 /////////////////////////////////////////////////////////////////////////
 
-`include "defs.svh"
+`include "def.svh"
 
 module RS #(
     parameter int unsigned RS_DEPTH        = 64, //RS entry numbers
@@ -20,46 +20,49 @@ module RS #(
     parameter int unsigned MAX_FU_PER_TYPE = 4,  // how many Fu per each FU type
     parameter int unsigned XLEN            = 64
 )(
-    input  logic                                                  clk,
-    input  logic                                                  reset,
+    input   logic                                                  clk,
+    input   logic                                                  reset,
+    input   logic                                                  flush,
 
     // =========================================================
     // Dispatch <-> RS
     // =========================================================
-    input  logic          [DISPATCH_WIDTH-1:0]                    disp_valid_i,
-    input  rs_entry_t     [DISPATCH_WIDTH-1:0]                    rs_packets_i,
-    input  logic          [DISPATCH_WIDTH-1:0]                    disp_rs_rd_wen_i,     // read (I think it is whether write PRF?)
+    input   logic          [DISPATCH_WIDTH-1:0]                    disp_valid_i,
+    input   rs_entry_t     [DISPATCH_WIDTH-1:0]                    rs_packets_i,
+    input   logic          [DISPATCH_WIDTH-1:0]                    disp_rs_rd_wen_i,     // read (I think it is whether write PRF?)
 
-    output logic          [$clog2(DISPATCH_WIDTH)-1:0]            free_slots_o,      // how many slot is free? (saturate at DISPATCH_WIDTH)
-    output logic                                                  rs_full_o,
+    output  logic          [$clog2(DISPATCH_WIDTH)-1:0]            free_slots_o,      // how many slot is free? (saturate at DISPATCH_WIDTH)
+    output  logic                                                  rs_full_o,
 
     // =========================================================
     // CDB -> RS 
     // =========================================================
-    input  logic          [CDB_WIDTH-1:0]                         cdb_valid_i, 
-    input  logic          [CDB_WIDTH-1:0][$clog2(PHYS_REGS)-1:0]  cdb_tag_i,
+    input   logic          [CDB_WIDTH-1:0]                         cdb_valid_i, 
+    input   logic          [CDB_WIDTH-1:0][$clog2(PHYS_REGS)-1:0]  cdb_tag_i,
 
     // =========================================================
-    // RS <-> FU (Issue)
+    // RS -> Issue logic (let Issue logic control who to issue)
     // =========================================================
-    input  logic          [$clog2(MAX_FU_PER_TYPE)-1:0]           fu_status_i [FU_NUM],
+    input   logic          [RS_DEPTH-1:0]                          issue_enable_i,
 
-    output logic          [ISSUE_WIDTH-1:0]                       issue_valid_o,
-    output issue_packet_t [ISSUE_WIDTH-1:0]                       issue_pkt_o,    // packet for [rs -> issue]
-    
+    output  rs_entry_t     [RS_DEPTH-1:0]                          rs_entries_o,
+    output  logic          [RS_DEPTH-1:0]                          rs_ready_o,  
+    output  logic          [$clog2(FU_NUM)-1:0]                    fu_type_o [RS_DEPTH]    
 ); 
+
+    // =========================================================
+    // Internal control signal
+    // =========================================================
     // Dispatch signal
     logic         [RS_DEPTH-1:0]             disp_enable;
     rs_entry_t    [RS_DEPTH-1:0]             rs_packets;
     logic         [RS_DEPTH-1:0]             rs_empty;
 
     // Issue signal
-    logic         [RS_DEPTH-1:0]             issue_enable;
-    logic         [RS_DEPTH-1:0]             rs_ready;  
-    logic         [$clog2(FU_NUM)-1:0]       fu_types [RS_DEPTH];
+    rs_entry_t    [RS_DEPTH-1:0]             rs_entries,
+    logic         [RS_DEPTH-1:0]             rs_ready,  
+    logic         [$clog2(FU_NUM)-1:0]       fu_types [RS_DEPTH],
 
-    logic         [RS_DEPTH-1:0]             rs_issue_valid;
-    issue_packet  [RS_DEPTH-1:0]             rs_issue_pkts;
 
     // Dispatch_grant_rs_slot
     logic [DISPATCH_WIDTH-1:0][RS_DEPTH-1:0] disp_grant_vec;
@@ -76,14 +79,14 @@ module RS #(
             )(
                 .clk(clk),
                 .reset(reset),
+                .flush(flush),
                 .disp_enable_i(disp_enable[i]),
                 .rs_packets_i(rs_packets[i]),
                 .empty_o(rs_empty[i]),
-                .issue_enable_i(issue_enable[i]),
-                .fu_type_o(fu_types[i]),
-                .ready_o(rs_ready[i]),
-                .rs_issue_valid_single_o(rs_issue_valid[i]),
-                .rs_issue_pkt_single_o(rs_issue_pkts[i]),
+                .issue_i(issue_enable_i[i]),
+                .rs_single_entry_o(rs_entries_o[i]),
+                .fu_type_o(fu_type_o[i]),
+                .ready_o(rs_ready_o[i]),
                 .cdb_valid_single_i(cdb_valid_i),
                 .cdb_tag_single_i(cdb_tag_i)
             );
@@ -91,10 +94,9 @@ module RS #(
     endgenerate
 
     // =========================================================
-    // Dispatch packet to RS entries
+    // Dispatch packets to RS entries
     // =========================================================
-
-    // selector: select which rs entry to dispatch
+    // Dispatch selector: select which rs entry to dispatch
     disp_selector disp_sel #(
         .RS_DEPTH(RS_DEPTH),
         .DISPATCH_WIDTH(DISPATCH_WIDTH)
@@ -104,7 +106,7 @@ module RS #(
         .disp_grant_vec(disp_grant_vec)
     );
 
-    // grant input packects to its corresponding rs entry (detemrine by rs_sel)
+    // Grant input packects to its corresponding rs entry (detemrine by rs_sel)
     always_comb begin: disp_pkt
         rs_packets  = '0;
         disp_enable = '0;
@@ -123,43 +125,6 @@ module RS #(
     end
 
     // =========================================================
-    // Grant Issue permission to RS entry (by issue selector)
-    // =========================================================
-
-    // Get 'issue_enable' sent to the rs entry
-    issue_selector issue_sel #(
-        .RS_DEPTH(RS_DEPTH),
-        .FU_NUM(FU_NUM),
-        .MAX_FU_PER_TYPE(MAX_FU_PER_TYPE),
-        .ISSUE_WIDTH(ISSUE_WIDTH)
-    )(
-        .fu_status_vec(fu_status_i),
-        .rs_ready_vec(rs_ready),
-        .fu_types(fu_types),
-        .issue_rs_entry(issue_enable) 
-    );
-
-    // Turn 'rs_issue_valid' & 'rs_issue_pkts" to output (issue_valid_o &　issue_pkt_o)
-    always_comb begin : issue_output
-        // Default outputs
-        issue_valid_o     = '0;
-        issue_opcode_o    = '0;
-        issue_dest_tag_o  = '0;
-        issue_opa_tag_o   = '0;
-        issue_opb_tag_o   = '0;
-
-        int out_idx = 0;
-
-        for (int i = 0; i < RS_DEPTH; i++) begin
-            if (rs_issue_valid[i] && (out_idx < ISSUE_WIDTH)) begin
-                issue_valid_o[out_idx] = 1'b1;
-                issue_pkt_o[out_idx]   = rs_issue_pkts[i];
-                out_idx++;
-            end
-        end
-    end
-
-    // =========================================================
     // Check remaining free slots (report to dispatch stage)
     // =========================================================
     always_comb begin : count_free_slot
@@ -173,6 +138,9 @@ module RS #(
         end
     end
 
+    // =========================================================
+    // Output
+    // =========================================================
     // assign results to output port
     assign rs_full_o    = rs_full;
     assign free_slots_o = free_slots;
