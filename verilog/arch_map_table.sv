f1d39e4b8f635fc5c6902d4bfc7dddb266964752
Zheng Lu <zhelu@caen-vnc-mi07.engin.umich.edu>
2025-10-27 18:47:35 -0400

    Hi

--- verilog/arch_map_table.sv
+++ verilog/arch_map_table.sv
@@ -39,12 +39,12 @@ module arch_map_table #(
     // =======================================================
     // Commit update
     // =======================================================
-    always_ff @(posedge clk && posedge reset)begin
+    always_ff @(posedge clk or posedge reset)begin
         if(reset)begin
             for(int i =0; i< ARCH_REGS; i++)begin
                     table[i] <= i;
             end
-        end else if (restore_snapshot_i)begin
+        end else if (restore_valid_i)begin
             for(int i =0; i< ARCH_REGS; i++)begin
                 table[i] <= restore_snapshot_i[i];
             end
